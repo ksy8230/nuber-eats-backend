2b100831445b17c2116822e05633e862d8a5e1e0
ksy <ksy8230@hanmail.net>
2020-12-24 17:03:10 +0900

    feat: joi 모듈로 환경변수 유효성 검사하기

--- README.md
+++ README.md
@@ -95,6 +95,7 @@ npm i --save @nestjs/config
 ```
 npm i cross-env
 ```
+- 환경변수 유효성 검사
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,21 @@ import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env",
       ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV : Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
     }),
     TypeOrmModule.forRoot({
       "type": "postgres",
