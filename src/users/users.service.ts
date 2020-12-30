3e2d08155405c1c08b09eafee1d6a831b59fa5ee
ksy <ksy8230@hanmail.net>
2020-12-30 15:18:45 +0900

    refactor: 에러처리 코드 더 간결하게

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,16 +14,17 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return '사용자가 이미 존재합니다.';
+        return { ok: false, error: '사용자가 이미 존재합니다.' };
       }
       // TODO : create user & hash the password
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return '계정을 생성할 수 없습니다.';
+      return { ok: false, error: '계정을 생성할 수 없습니다.' };
     }
   }
 }
