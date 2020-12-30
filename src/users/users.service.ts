9512bf9ed91689f85e686f976eaf76f86e99d43e
ksy <ksy8230@hanmail.net>
2020-12-30 15:05:56 +0900

    feat: 유저 생성 / 에러 다루기

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,21 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        return '사용자가 이미 존재합니다.';
+      }
+      // TODO : create user & hash the password
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (e) {
+      return '계정을 생성할 수 없습니다.';
+    }
+  }
 }
