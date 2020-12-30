66fde9bd6b4d9d04b39d081cdecb13086e4b671c
ksy <ksy8230@hanmail.net>
2020-12-30 14:42:46 +0900

    feat: 유저 정보 중 role type이 grahpql과 db가 둘 다 같은 enum type 을 바라보도록 수정

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,19 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
 @InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
@@ -15,7 +26,7 @@ export class User extends CoreEntity {
   @Field(() => String)
   password: string;
 
-  @Column()
-  @Field(() => String)
+  @Column({ type: 'enum', enum: UserRole }) // DB : type이 UserRole
+  @Field(() => UserRole) // graphql : type이 UserRole
   role: UserRole;
 }
