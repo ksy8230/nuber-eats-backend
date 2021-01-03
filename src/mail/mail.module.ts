b415509b14707107a7874daa9873127b2587c57a
ksy <ksy8230@hanmail.net>
2021-01-03 09:06:23 +0900

    feat: mailgun api 사용하기

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { MailModuleOptions } from './mail.interfaces';
 import { CONFIG_OPTIONS } from 'src/jwt/jwt.constants';
+import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
@@ -12,8 +13,9 @@ export class MailModule {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
+        MailService,
       ],
-      exports: [],
+      exports: [MailService],
     };
   }
 }
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,37 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/jwt/jwt.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    console.log('a', options);
+    this.sendEmail('testing', 'test');
+  }
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `mailgun@${this.options.domain}`);
+    form.append('to', `${this.options.fromEmail}`);
+    form.append('subject', subject);
+    form.append('content', content);
+    form.append('html', `<h1>Test body</h2>`);
+
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log('a', response.body);
+  }
+}
