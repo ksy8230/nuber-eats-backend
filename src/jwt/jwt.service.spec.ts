fca63e68d89553f280c7baae1bc55e107c461a60
ksy <ksy8230@hanmail.net>
2021-01-16 09:57:54 +0900

    test: jwt 서비스 테스트 셋업

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,27 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtService } from './jwt.service';
+
+const TEST_KEY = 'test_key';
+
+describe('JwtService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  it.todo('sign');
+  it.todo('verify');
+});
