8be44375e20078fdf6ed82156e5f162687027945
ksy <ksy8230@hanmail.net>
2021-01-17 15:24:39 +0900

    test : e2e - 유저 이미 있는 경우 resolver 함수 테스트 (호출 테스트)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,13 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+// 실제로 유저 생성을 (post)호출하면 이메일 인증 코드가 테스트 할 때마다 날아오기 때문에 post 를 모킹한다
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UsersModule (e2e)', () => {
@@ -25,22 +32,46 @@ describe('UsersModule (e2e)', () => {
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `
-          mutation {
-            createAccount(input: {
-              email: "${EMAIL}",
-              password: "1234",
-              role:Owner
-            }) {
-              ok
-              error
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "1234",
+                role:Owner
+              }) {
+                ok
+                error
+              }
             }
-          }
-        `,
+          `,
         })
         .expect(200)
         .expect((res) => {
           expect(res.body.data.createAccount.ok).toBe(true);
           expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account is existed', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "1234",
+                role:Owner
+              }) {
+                ok
+                error
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
         });
     });
   });
