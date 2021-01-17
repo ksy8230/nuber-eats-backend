faf3773ccea840e09369e0a916afa01ef5bc5c52
ksy <ksy8230@hanmail.net>
2021-01-17 15:13:05 +0900

    test : e2e - 유저 생성 resolver 함수 테스트 (호출 테스트)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UsersModule (e2e)', () => {
   let app;
@@ -15,5 +18,40 @@ describe('UsersModule (e2e)', () => {
     await app.init();
   });
 
+  describe('createAccount', () => {
+    const EMAIL = 'mollog@gmail.com';
+    it('should create user account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "1234",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+  });
+  it.todo('userProfile');
+  it.todo('login');
   it.todo('me');
+  it.todo('verifyemail');
+  it.todo('editProfile');
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
 });
