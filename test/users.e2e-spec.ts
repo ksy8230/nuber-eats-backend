9200a1a5b36b500e0a2e8ef5cfaf66d6a2575953
ksy <ksy8230@hanmail.net>
2021-01-18 21:46:44 +0900

    test : e2e - 로그인 함수 테스트 (호출 테스트)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -12,9 +12,14 @@ jest.mock('got', () => {
 });
 
 const GRAPHQL_ENDPOINT = '/graphql';
+const testUser = {
+  email: 'mollog@gmail.com',
+  password: '1234',
+};
 
 describe('UsersModule (e2e)', () => {
   let app;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,8 +39,8 @@ describe('UsersModule (e2e)', () => {
           query: `
             mutation {
               createAccount(input: {
-                email: "${EMAIL}",
-                password: "1234",
+                email: "${testUser.email}",
+                password: "${testUser.password}",
                 role:Owner
               }) {
                 ok
@@ -75,8 +80,69 @@ describe('UsersModule (e2e)', () => {
         });
     });
   });
+
+  describe('login', () => {
+    it('should login with correct credential', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input : {
+              email: "${testUser.email}",
+              password: "${testUser.password}",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+    it('should not be able to login with wrond credential', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input : {
+              email: "${testUser.email}",
+              password: "xxx",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('비밀번호가 틀렸습니다.');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyemail');
   it.todo('editProfile');
