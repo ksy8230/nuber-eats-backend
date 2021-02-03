88d22b49e3e62f50588971117e6dc2165636ef79
ksy <ksy8230@hanmail.net>
2021-02-04 00:22:37 +0900

    feat: 주문 생성 서비스 로직에서 주문 가격 계산

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -32,18 +32,51 @@ export class OrderService {
         error: 'Restaurant not found',
       };
     }
-    items.forEach(async (item) => {
+
+    for (const item of items) {
       const dish = await this.dishes.findOne(item.dishId);
       if (!dish) {
-        // abort this whole thing
+        return {
+          ok: false,
+          error: 'Dish not found.',
+        };
       }
-      await this.orderItems.save(
+      console.log(`Dish price: ${dish.price}`);
+      // 주문 생성에 넣은 각 옵션들 배회하며 금액 계산
+      for (const itemOption of item.options) {
+        /**
+         * 메뉴 옵션에서 db에 있는 옵션명 = 주문 생성에 입력된 옵션명이 같은 메뉴 옵션 찾기
+         */
+        const dishOption = dish.options.find(
+          (dishOption) => dishOption.name === itemOption.name,
+        );
+        if (dishOption) {
+          // 찾은 메뉴 옵션에 extra 프로퍼티 값 얻기
+          if (dishOption.extra) {
+            console.log(`$USD + ${dishOption.extra}`);
+          } else {
+            /**
+             * 메뉴 옵션의 초이스들에서 초이스의 이름 = 주문 생성에 입력된 초이스가 같은 메뉴 옵션 초이스 찾기
+             */
+            const dishOptionChoice = dishOption.choices.find(
+              (optionChoice) => optionChoice.name === itemOption.choice,
+            );
+            // 찾은 메뉴 옵션의 초이스에 extra 프로퍼티 값 얻기
+            if (dishOptionChoice) {
+              if (dishOptionChoice.extra) {
+                console.log(`$USD + ${dishOptionChoice.extra}`);
+              }
+            }
+          }
+        }
+      }
+      /*await this.orderItems.save(
         this.orderItems.create({
           dish,
           options: item.options,
         }),
-      );
-    });
+      ); */
+    }
     /* const order = await this.orders.save(
       this.orders.create({
         customer,
