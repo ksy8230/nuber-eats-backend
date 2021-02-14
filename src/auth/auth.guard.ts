987221f0b1d68acd9bf691a9bd14228a774bce0b
ksy <ksy8230@hanmail.net>
2021-02-14 20:08:01 +0900

    feate: auth 데코레이터에서 user context 불러오기

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,32 +2,40 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtService } from 'src/jwt/jwt.service';
+import { UsersService } from 'src/users/users.service';
 import { AllowedRoles } from './role.decorator';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
     private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
   ) {}
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
     );
-    console.log(roles);
+
     if (!roles) {
       return true;
     }
     const graphqlContext = GqlExecutionContext.create(context).getContext();
-    console.log('graphqlContext = ', graphqlContext.token);
-    const user = graphqlContext['user'];
-
-    if (!user) {
-      return false;
+    console.log('graphqlContext = ', graphqlContext);
+    const token = graphqlContext.token;
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        if (user) {
+          graphqlContext['user'] = user;
+          if (roles.includes('Any')) {
+            return true;
+          }
+          return roles.includes(user.role);
+        }
+      }
     }
-    if (roles.includes('Any')) {
-      return true;
-    }
-    return roles.includes(user.role); // true
+    return false;
   }
 }
